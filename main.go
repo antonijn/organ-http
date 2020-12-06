011aa0b88f678a1e14cd51967a2668881527b9fd
Antonie Blom <ablom@posteo.net>
2020-12-06 17:07:03 +0100

    Add recording rename functionality

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"path"
+	"sort"
 )
 
 type Configuration struct {
@@ -40,65 +40,119 @@ func getAppConfig() {
 
 	err := readJsonConfig(globalConfigPath, &appConfig)
 	if err != nil {
-		log.Println(err)
+		log.Print(err)
 	}
 	err = readJsonConfig(localConfigPath, &appConfig)
 	if err != nil {
-		log.Println(err)
+		log.Print(err)
 	}
 }
+
+const javaScriptRenameCode = `
+function showRenamePrompt(filename) {
+	newname = prompt('Enter new file name', filename);
+	if (filename != null) {
+		document.getElementById(filename + '-newname').value = newname;
+		return true;
+	}
+
+	return false;
+}`
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 
 		title := html.EscapeString(appConfig.OrganName)
-		fmt.Fprintf(w, "<html><head><title>%s</title></head><body>", title)
-		fmt.Fprintf(w, "<h1>%s</h1>", title)
+
+		fmt.Fprintln(w, "<html>")
+		fmt.Fprintln(w, "<head>")
+		fmt.Fprintf(w, "<title>%s</title>\n", title)
+		fmt.Fprintln(w, "<script>")
+		fmt.Fprintln(w, javaScriptRenameCode)
+		fmt.Fprintln(w, "</script>")
+		fmt.Fprintln(w, "</head>")
+		fmt.Fprintln(w, "<body>")
+		fmt.Fprintf(w, "<h1>%s</h1>\n", title)
 
 		files, err := ioutil.ReadDir(appConfig.RecordingsDir)
 		if err != nil {
 			fmt.Fprintln(w, "<p>Error: recordings inaccessible</p>")
 		} else {
+			sort.Slice(files, func(i,j int) bool{ return files[i].ModTime().After(files[j].ModTime()) })
+
 			fmt.Fprintln(w, "<h2>Recordings</h2>")
-			fmt.Fprintln(w, "<table><tr><th>Download</th><th>Size (MiB)</th><th>Delete</th></tr>")
+			fmt.Fprintln(w, "<table><tr><th>Download</th><th>Size (MiB)</th><th>Rename</th><th>Delete</th></tr>")
 
 			for _, file := range files {
 				filename := file.Name()
-				filePath := path.Join("/audio/", filename)
-				fileUrl, _ := url.Parse(filePath)
+				escFileName := html.EscapeString(filename)
 				fmt.Fprintln(w, "<tr>")
-				fmt.Fprintf(w, "<td><a href=%s>%s</a></td><td>%.1f</td>", fileUrl, html.EscapeString(filename), float32(file.Size()) / (1024 * 1024))
-				fmt.Fprintf(w, "<td><form method=\"post\" onsubmit=\"return confirm('Are you sure?');\"><button type=\"submit\" name=\"deleterecording\" value=%s>delete</button></td>", html.EscapeString(filename))
+
+				fmt.Fprintf(w, "<td><a href=\"/audio/%s\">%s</a></td>\n", escFileName, escFileName)
+				fmt.Fprintf(w, "<td>%.1f</td>\n", float32(file.Size()) / (1024 * 1024))
+
+				fmt.Fprintln(w, "<td>")
+				fmt.Fprintln(w, "<form method=\"post\" onsubmit=\"return confirm('Are you sure?');\">")
+				fmt.Fprintf(w, "<button type=\"submit\" name=\"deleterecording\" value=\"%s\">delete</button>\n", escFileName)
+				fmt.Fprintln(w, "</form>")
+				fmt.Fprintln(w, "</td>")
+
+				fmt.Fprintln(w, "<td>")
+				fmt.Fprintln(w, "<form method=\"post\">")
+				fmt.Fprintf(w, "<input type=\"hidden\" name=\"newname\" id=\"%s-newname\">\n", escFileName)
+				fmt.Fprintf(w, "<button type=\"submit\" name=\"renamerecording\" value=\"%s\" onClick=\"return showRenamePrompt('%s')\">rename</button>\n", escFileName, escFileName)
+				fmt.Fprintln(w, "</form>")
+				fmt.Fprintln(w, "</td>")
+
 				fmt.Fprintln(w, "</tr>")
 			}
 
 			fmt.Fprintln(w, "</table>")
 		}
-		fmt.Fprintln(w, "</body></html>")
+		fmt.Fprintln(w, "</html>")
+		fmt.Fprintln(w, "</body>")
 		break
 
 	case "POST":
 		defer http.Redirect(w, r, "/", http.StatusSeeOther)
 		r.ParseForm()
 		delRec := r.Form.Get("deleterecording")
-		if delRec == "" {
-			log.Println("Unhandled POST request")
-			return
+		if delRec != "" {
+			if path.Base(delRec) != delRec {
+				log.Println("Invalid resource delete attempt")
+				return
+			}
+
+			recPath := path.Join(appConfig.RecordingsDir, delRec)
+
+			if err := os.Remove(recPath); err != nil {
+				log.Print(err)
+				return
+			}
+
+			break
 		}
 
-		if path.Base(delRec) != delRec {
-			log.Println("Invalid resource delete attempt")
-			return
+		renameRec := r.Form.Get("renamerecording")
+		if renameRec != "" {
+			newname := r.Form.Get("newname")
+			if path.Base(renameRec) != renameRec || path.Base(newname) != newname {
+				log.Println("Invalid resource rename attempt")
+				return
+			}
+
+			oldPath := path.Join(appConfig.RecordingsDir, renameRec)
+			newPath := path.Join(appConfig.RecordingsDir, newname)
+			if err := os.Rename(oldPath, newPath); err != nil {
+				log.Print(err)
+				return
+			}
+
+			break
 		}
 
-		recPath := path.Join(appConfig.RecordingsDir, delRec)
-
-		if err := os.Remove(recPath); err != nil {
-			log.Print(err)
-			return
-		}
-
+		log.Println("Unhandled POST request")
 		break
 	}
 }
